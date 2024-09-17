5e9be539ad7bf3b155bee4d9f353e71a085d0e0d
ivababukova <iva@biomage.net>
2024-09-17 13:22:47 +0200

    fixed my apollo client

--- src/ApolloClient.js
+++ src/ApolloClient.js
@@ -1,35 +1,27 @@
-import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client';
-import { getMainDefinition } from '@apollo/client/utilities';
-import { WebSocketLink } from '@apollo/client/link/ws';
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  split,
+} from "@apollo/client";
+
 import { setContext } from '@apollo/client/link/context';
-import fetch from 'cross-fetch';
+import { createClient } from "graphql-ws";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { getMainDefinition } from "@apollo/client/utilities";
+
 
 const httpLink = new HttpLink({
   uri: process.env.REACT_APP_GRAPHQL_API_URL,
-  fetch,
 });
 
-const wsLink = new WebSocketLink({
-  uri: process.env.REACT_APP_GRAPHQL_WS_URL,
-  options: {
-    reconnect: true,
-    connectionParams: {
-      authorization: `Bearer ${process.env.REACT_APP_AUTH_TOKEN}`,
-    },
-  },
-});
 
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    );
-  },
-  wsLink,
-  httpLink
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: process.env.REACT_APP_GRAPHQL_API_URL, // backend link, check backend console for link
+  })
 );
+
 
 const authLink = setContext((_, { headers }) => {
   return {
@@ -40,9 +32,24 @@ const authLink = setContext((_, { headers }) => {
   }
 });
 
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    console.log("*** query def: ", definition);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  authLink.concat(wsLink),
+  authLink.concat(httpLink),
+);
+
+
 const client = new ApolloClient({
-  link: authLink.concat(splitLink),
-  cache: new InMemoryCache()
+  link: splitLink,
+  cache: new InMemoryCache(),
 });
 
 export default client;
