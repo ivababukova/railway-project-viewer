64a9c81c61e4e4f2c42874bd2a665be6699ad8b5
ivababukova <iva@biomage.net>
2024-09-18 10:04:58 +0200

    clean up unused subscriptions code

--- src/ApolloClient.js
+++ src/ApolloClient.js
@@ -2,28 +2,15 @@ import {
   ApolloClient,
   HttpLink,
   InMemoryCache,
-  split,
 } from "@apollo/client";
 
 import { setContext } from '@apollo/client/link/context';
-import { createClient } from "graphql-ws";
-import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
-import { getMainDefinition } from "@apollo/client/utilities";
 
 
 const httpLink = new HttpLink({
   uri: process.env.REACT_APP_GRAPHQL_API_URL,
 });
 
-
-const wsLink = new GraphQLWsLink(
-  createClient({
-    url: 'ws://backboard.railway.app',
-    connectionParams: {
-      authToken: process.env.REACT_APP_AUTH_TOKEN,
-    },
-  })
-);
 
 
 const authLink = setContext((_, { headers }) => {
@@ -36,22 +23,8 @@ const authLink = setContext((_, { headers }) => {
 });
 
 
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    );
-  },
-  wsLink,
-  authLink.concat(httpLink),
-);
-
-
 const client = new ApolloClient({
-  uri: '/graphql',
-  link: splitLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
