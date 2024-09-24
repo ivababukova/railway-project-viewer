f3e3bd1d61b2b3aa9b4d4867b54db83e463b2770
ivababukova <iva@biomage.net>
2024-09-24 20:07:15 +0200

    update with server's endpoint

--- src/ApolloClient.js
+++ src/ApolloClient.js
@@ -9,9 +9,10 @@ import { getToken } from './token.js';
 
 
 const setUrl = () => {
-  const url = process.env.RAILWAY_PUBLIC_DOMAIN || "http://localhost:4000";
-  console.log("URL used is: ", url);
-  return `${url}/graphql`;
+  if (process.env.NODE_ENV === 'development') {
+    return 'http://localhost:4000/graphql';
+  }
+  return "https://project-viewer-server-production.up.railway.app//graphql";
 }
 
 const httpLink = new HttpLink({
